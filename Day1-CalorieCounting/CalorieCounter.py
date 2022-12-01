1bb1a97e1f840cc4261081212d94ac36ed441599
aldrinn <48447138+aldringordon@users.noreply.github.com>
2022-12-01 13:41:28 +0800

    finish: p1 and p2

--- Day1-CalorieCounting/CalorieCounter.py
+++ Day1-CalorieCounting/CalorieCounter.py
@@ -11,15 +11,57 @@ TEST_ANSWER = 4
 TEST_ANSWER_INDEX = TEST_ANSWER - 1
 TEST_ANSWER_AMOUNT = 24000
 
+DATA_FILE = "Data.txt"
+
+def read_data(filename):
+    total = []
+    cals = []
+
+    f = open(filename, "r")
+    for l in f:
+        if l.strip():
+            cals.append(int(l))
+        else:
+            total.append(cals)
+            cals = []
+
+    f.close()
+    return total
+
 def count_calories(inputList):
     caloriesDict = {}
 
     for i, val in enumerate(inputList):
-        print(str(i) + ' ' + str(sum(val)))
         caloriesDict.update({ i+1: sum(val) })
 
     return caloriesDict
 
 if __name__=="__main__":
-    caloriesDict = count_calories(TEST_INPUT)
-    
+    inputData = read_data(DATA_FILE)
+
+    caloriesDict = count_calories(inputData)
+
+    val1 = max(caloriesDict.values())
+    maxKey = max(caloriesDict, key=caloriesDict.get)
+    print(maxKey)
+    print(caloriesDict[maxKey])
+    print
+
+    del caloriesDict[maxKey]
+
+    val2 = max(caloriesDict.values())
+    maxKey = max(caloriesDict, key=caloriesDict.get)
+    print(maxKey)
+    print(caloriesDict[maxKey])
+    print
+
+    del caloriesDict[maxKey]
+
+    val3 = max(caloriesDict.values())
+    maxKey = max(caloriesDict, key=caloriesDict.get)
+    print(maxKey)
+    print(caloriesDict[maxKey])
+    print
+
+    print('total:')
+    print(val1+val2+val3)
