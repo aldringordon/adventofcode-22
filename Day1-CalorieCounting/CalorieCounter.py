1979ba72d035d9b9bd32051cd46def68f16339db
aldrinn <48447138+aldringordon@users.noreply.github.com>
2022-12-01 13:18:24 +0800

    init: test data

---
+++ Day1-CalorieCounting/CalorieCounter.py
@@ -0,0 +1,15 @@
+
+TEST_INPUT = [
+    [1000, 2000, 3000,],
+    [4000],
+    [5000, 6000],
+    [7000, 8000, 9000],
+    [10000]
+    ]
+
+TEST_ANSWER = 4
+TEST_ANSWER_INDEX = TEST_ANSWER - 1
+TEST_ANSWER_AMOUNT = 24000
+
+if __name__=="__main__":
+    print("Hello world")
