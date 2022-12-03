f1085dd349595f9ce5e171799c6b1073e135994e
aldrinn <48447138+aldringordon@users.noreply.github.com>
2022-12-03 18:21:30 +0800

    ROCKPAPERSCISSSSS

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 from Program.CalorieCounter import CalorieCounter
+from Program.RockPaperScissors import RockPaperScissors
 
 def max_calories():
     cc = CalorieCounter()
@@ -9,8 +10,21 @@ def max_calories():
     cc.remove_max()
     print(cc.get_max_val())
 
+def rock_paper_sciss():
+    rps = RockPaperScissors()
+
+    print("Reading RPS Data")
+    rps.read_data("Day2-RockPaperScissors/Data.txt")
+
+    print("Calculating Score")
+    score = rps.calc_score()
+
+    print("Score: ")
+    print(score)
+
 def main():
-    max_calories()
+    #max_calories()
+    rock_paper_sciss()
 
 if __name__=="__main__":
     main()
