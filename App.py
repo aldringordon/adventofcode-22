1534ebdae883b23a525ca9ed882293749559970c
aldrinn <48447138+aldringordon@users.noreply.github.com>
2022-12-03 18:45:14 +0800

    Rucksack Organiser

--- App.py
+++ App.py
@@ -1,12 +1,15 @@
 from Program.CalorieCounter import CalorieCounter
 from Program.RockPaperScissors import RockPaperScissors
+from Program.RucksackOrganiser import RucksackOrganiser
 
 def max_calories():
     cc = CalorieCounter()
     cc.read_and_convert("Day1-CalorieCounting/Data.txt")
     print(cc.get_max_val())
+
     cc.remove_max()
     print(cc.get_max_val())
+
     cc.remove_max()
     print(cc.get_max_val())
 
@@ -22,9 +25,22 @@ def rock_paper_sciss():
     print("Score: ")
     print(score)
 
+def rucksack_organiser():
+    rso = RucksackOrganiser()
+    rso.read_data("Day3-RucksackOrganization/Data.txt")
+    common_item_sum = rso.get_common_item_priority_sum()
+    print("common item sum:")
+    print(common_item_sum)
+
+    common_group_item_sum = rso.get_group_item_priority_sum()
+    print("group badges sum:")
+    print(common_group_item_sum)
+
 def main():
     #max_calories()
-    rock_paper_sciss()
+    #rock_paper_sciss()
+    #rucksack_organiser()
+    pass
 
 if __name__=="__main__":
     main()
