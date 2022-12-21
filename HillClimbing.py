6cb525cdc000cf95eecd1e615e0a4763e4223be2
aldrinn <48447138+aldringordon@users.noreply.github.com>
2022-12-21 22:17:36 +0800

    trying

--- HillClimbing.py
+++ HillClimbing.py
@@ -6,9 +6,58 @@ a = lowest
 z = highest
 
 Move up, down, left, right
+
+Shortest Path
+https://www.bogotobogo.com/python/python_Dijkstras_Shortest_Path_Algorithm.php
 '''
 
 from Map import Map
+from Djikstra import Graph
+import heapq
+
+def shortest(v, path):
+    ''' make shortest path from v.previous'''
+    if v.previous:
+        path.append(v.previous.get_id())
+        shortest(v.previous, path)
+    return
+
+def dijkstra(aGraph, start, target):
+    print("Dijkstra's shortest path")
+    # Set the distance for the start node to zero 
+    start.set_distance(0)
+
+    # Put tuple pair into the priority queue
+    unvisited_queue = [(v.get_distance(),v) for v in aGraph]
+    heapq.heapify(unvisited_queue)
+
+    while len(unvisited_queue):
+        # Pops a vertex with the smallest distance 
+        uv = heapq.heappop(unvisited_queue)
+        current = uv[1]
+        current.set_visited()
+
+        #for next in v.adjacent:
+        for next in current.adjacent:
+            # if visited, skip
+            if next.visited:
+                continue
+            new_dist = current.get_distance() + current.get_weight(next)
+            
+            if new_dist < next.get_distance():
+                next.set_distance(new_dist)
+                next.set_previous(current)
+                print("updated : current = %s next = %s new_dist = %s" % current.get_id(), next.get_id(), next.get_distance())
+            else:
+                print("not updated : current = %s next = %s new_dist = %s" % current.get_id(), next.get_id(), next.get_distance())
+
+        # Rebuild heap
+        # 1. Pop every item
+        while len(unvisited_queue):
+            heapq.heappop(unvisited_queue)
+        # 2. Put all vertices not visited into the queue
+        unvisited_queue = [(v.get_distance(),v) for v in aGraph if not v.visited]
+        heapq.heapify(unvisited_queue)
 
 def read_data(filename):
     data = []
@@ -25,6 +74,24 @@ def main():
     map = Map(data)
     
     map.print_map()
+
+    map.create_graph()
+
+    g = map.get_graph()
+
+    print('graph')
+    for v in g:
+        for w in v.get_connections():
+            vid = v.get_id()
+            wid = w.get_id()
+            print (f"( {vid} , {wid})")
+    
+    dijkstra(g, g.get_vertex('a'), g.get_vertex('e')) 
+
+    target = g.get_vertex('e')
+    path = [target.get_id()]
+    shortest(target, path)
+    print(f"The shortest path : {(path[::-1])}")
 
 if __name__=='__main__':
     main()
--- Map.py
+++ Map.py
@@ -1,11 +1,15 @@
+from Djikstra import Graph
+
+COST = 1
+
 class Map():
 
     def __init__(self, map):
         self.map = map
         self.current = (0, 0)
         self.target = (0, 0)
-
         self.setup()
+        self.g = Graph()
     
     def setup(self):
         for i, row in enumerate(self.map):
@@ -28,3 +32,46 @@ class Map():
         else:
             for x in self.map:
                 print(('{}'*len(x)).format(*x))
+
+    def can_move(self, source, dest):
+        if ord(source) - ord(dest) == -1:
+            return True
+        return False
+
+    def get_moves(self, i, j):
+        x = self.map[i][j]
+
+        # Up
+        if i > 0:
+            if self.can_move(x, self.map[i-1][j]):
+                self.g.add_edge(x, self.map[i-1][j], COST)
+
+        # Down
+        if i < len(self.map) - 1:
+            if self.can_move(x, self.map[i+1][j]):
+                self.g.add_edge(x, self.map[i+1][j], COST)
+
+        # Left
+        if j > 0:
+            if self.can_move(x, self.map[i][j-1]):
+                self.g.add_edge(x, self.map[i][j-1], COST)
+
+        # Right
+        if j < len(self.map[0]) - 1:
+            if self.can_move(x, self.map[i][j+1]):
+                self.g.add_edge(x, self.map[i][j+1], COST)
+
+    def get_graph(self):
+        return self.g
+
+    def create_graph(self):
+        
+        # Create Vertices
+        for row in self.map:
+            for v in row:
+                self.g.add_vertex(v)
+
+        # Create Edges
+        for i, row in enumerate(self.map):
+            for j in range(len(row)):
+                self.get_moves(i, j)
